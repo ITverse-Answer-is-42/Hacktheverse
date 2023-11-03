371fb4b0ac793db5d19e5faac1d917bf03e189e9
2018331113 <muhitmahmud404@gmail.com>
2023-11-03 19:33:17 +0600

    updated searchpage

--- itverse_frontend/lib/screens/searchpage.dart
+++ itverse_frontend/lib/screens/searchpage.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:itverse_frontend/constants/api_path.dart';
-import 'package:itverse_frontend/util/helper/auto_complete.dart';
-import 'package:uuid/uuid.dart';
-import 'package:http/http.dart' as http;
+
+import '../widgets/custom_search.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -14,48 +10,30 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final TextEditingController _controller = TextEditingController();
   @override
   void initState() {
-    _controller.addListener(() {
-      onChange();
-    });
     super.initState();
-  }
-
-  final AutoCompleteService _autoComplete = AutoCompleteService();
-  List<String> suggestions = [];
-  void onChange() async {
-    suggestions = await _autoComplete.getSuggestions(_controller.text);
-    setState(() {
-      if (_controller.text.isEmpty) {
-        suggestions = [];
-      }
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: CustomSearchDelegate());
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          TextField(
-            controller: _controller,
-            decoration: const InputDecoration(
-              hintText: "Search by City/Country",
-              prefixIcon: Icon(Icons.search),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: suggestions.length,
-                itemBuilder: (context, index) {
-                  return Text(suggestions[index]);
-                }),
-          )
+          Container(),
         ],
       ),
     );
   }
 }
+
