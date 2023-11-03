20b8b216115e184344ed2b9bd14f933aa6028220
2018331113 <muhitmahmud404@gmail.com>
2023-11-03 19:31:50 +0600

    added constants

--- itverse_frontend/lib/constants/api_path.dart
+++ itverse_frontend/lib/constants/api_path.dart
@@ -1,5 +1,7 @@
 const kGoogleMapsApi = "AIzaSyDl2Rtz9pi5LOc-LbBQ-3OxPfYFgb_ktVA";
 
-const kMapsUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
+const kAutoCompleteUrl =
+    "https://maps.googleapis.com/maps/api/place/autocomplete/json";
 
+const kGeoCodingUrl = "https://maps.googleapis.com/maps/api/geocode/json";
 const kHereUrl = "http://10.100.201.14:4000/api/v1/aqi";
---
+++ itverse_frontend/lib/constants/asset_path.dart
@@ -0,0 +1,7 @@
+const imageAssetPath = "assets/images";
+const kGoodIcon = "$imageAssetPath/good.png";
+const kModerateIcon = "$imageAssetPath/moderate.png";
+const kUnhealthyForSensitiveIcon = "$imageAssetPath/sensitive.png";
+const kUnhealthyIcon = "$imageAssetPath/unhealthy.png";
+const kVeryUnhealthyIcon = "$imageAssetPath/very_unhealthy.png";
+const kHazardousIcon = "$imageAssetPath/hazzard.png";
