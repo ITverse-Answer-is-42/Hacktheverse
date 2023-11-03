37ab031aac0f93b34303f7da1b2d846eaad64315
2018331113 <muhitmahmud404@gmail.com>
2023-11-03 19:34:22 +0600

    refactor

--- itverse_frontend/lib/util/helper/auto_complete.dart
+++ itverse_frontend/lib/util/helper/auto_complete.dart
@@ -2,16 +2,18 @@ import 'dart:convert';
 
 import '../../constants/api_path.dart';
 import 'package:http/http.dart' as http;
-class AutoCompleteService{
 
-  Future<List<String>> getSuggestions(String query)async {
+class AutoCompleteService {
+  Future<List<String>> getSuggestions(String query) async {
     String url =
-        "$kMapsUrl?input=$query&key=$kGoogleMapsApi&type=locality";
+        "$kAutoCompleteUrl?input=$query&key=$kGoogleMapsApi&type=locality";
     final response = await http.get(Uri.parse(url));
 
     final result = jsonDecode(response.body);
 
-    List<String> suggestions = result['predictions'].map<String>((e) => e['description'] as String).toList();
+    List<String> suggestions = result['predictions']
+        .map<String>((e) => e['description'] as String)
+        .toList();
 
     return suggestions;
   }
